46b80fc2ec28322269fe42efa5147b174beb5ce5
vishnu-git1524 <vishnuvenkat1524@gmail.com>
2024-06-14 09:44:10 +0530

    atlas

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('download-btn').addEventListener('click', function() {
+    window.location.href = 'https://your-download-link.com/your-app.apk';
+});
+
+document.getElementById('navbar-download-btn').addEventListener('click', function() {
+    window.location.href = 'https://your-download-link.com/your-app.apk';
+});
