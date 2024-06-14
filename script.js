06557f51faf09079a5701c0f05b6e071cc9f2b8a
vishnu-git1524 <vishnuvenkat1524@gmail.com>
2024-06-14 10:01:07 +0530

    atlas_link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('download-btn').addEventListener('click', function() {
-    window.location.href = 'https://your-download-link.com/your-app.apk';
+    window.location.href = 'https://github.com/vishnu-git1524/atlas_web/releases/download/1.0/atlas.apk';
 });
 
 document.getElementById('navbar-download-btn').addEventListener('click', function() {
-    window.location.href = 'https://your-download-link.com/your-app.apk';
+    window.location.href = 'https://github.com/vishnu-git1524/atlas_web/releases/download/1.0/atlas.apk';
 });
