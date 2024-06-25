14250cd4b0e149d9eec7c45f24aa0406492c9222
vishnu-git1524 <vishnuvenkat1524@gmail.com>
2024-06-25 08:17:16 +0530

    updated apk

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-document.getElementById('download-btn').addEventListener('click', function() {
+document.getElementById('download-btn-1').addEventListener('click', function() {
     window.location.href = 'https://github.com/vishnu-git1524/atlas_web/releases/download/1.0/atlas.apk';
 });
 
-document.getElementById('navbar-download-btn').addEventListener('click', function() {
-    window.location.href = 'https://github.com/vishnu-git1524/atlas_web/releases/download/1.0/atlas.apk';
+document.getElementById('download-btn-2').addEventListener('click', function() {
+    window.location.href = 'https://github.com/vishnu-git1524/atlas_web/releases/download/1.0/atlas_v8.apk';
+});
+
+document.getElementById('download-btn-3').addEventListener('click', function() {
+    window.location.href = 'https://github.com/vishnu-git1524/atlas_web/releases/download/1.0/app_86_64.apk';
 });
